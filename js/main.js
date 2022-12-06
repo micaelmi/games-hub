cdadb3895dfb6db6a15d070ee349c520dee4be36
garradefogo124 <micaelmiranda124@gmail.com>
2022-12-06 17:56:13 -0300

    Games Hub - versão 1

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const cardContainer = document.querySelector('.card-container');
+
+window.onload = () => {
+    renderGames()
+}
+
+let games = [
+    {
+        img: "./img/memory-game.png",
+        title: "Jogo da Memória",
+        description: "Um clássico jogo da memória no estilo Pokémon, encontre os pares o mais rápido possível.",
+        url: "https://micaelmi.github.io/memory-game/"
+    },
+    {
+        img: "./img/snake-game.png",
+        title: "Jogo da Cobrinha",
+        description: "Um dos jogos mais antigos, na aparência dos anos 80. Cresça o máximo possível",
+        url: "https://micaelmi.github.io/snake-game/"
+    },
+    {
+        img: "./img/tic-tac-toe.png",
+        title: "Jogo da Velha",
+        description: "Quem não conhece esse clássico? É só encontrar sua dupla e jogar.",
+        url: "https://micaelmi.github.io/tic-tac-toe/"
+    },
+    {
+        img: "./img/mario-jump.png",
+        title: "Mario Jump",
+        description: "Uma mistura do joguinho do dinossauro com nosso herói Mario.",
+        url: "https://micaelmi.github.io/mario-jump-game/"
+    },
+]
+
+function renderGames() {
+    games.map((game) => {
+        cardContainer.innerHTML +=
+            `
+        <div class="card">
+          <div class="thumb">
+            <img src="${game.img}" alt="Capa '${game.title}'" />
+          </div>
+          <h2>${game.title}</h2>
+          <p>${game.description}</p>
+          <a href="${game.url}" target="_blank">Jogar</a>
+        </div>
+        `
+    })
+}
